f20c61ac563ae8ef5f90be9384f5a8843bec0105
frasercrossman <frasercrossman@outlook.com>
2017-03-11 16:20:51 +0000

    Added loginRequest method

--- app/src/main/java/ourcoolgroup/facebookhack/MainActivity.java
+++ app/src/main/java/ourcoolgroup/facebookhack/MainActivity.java
@@ -2,6 +2,7 @@ package ourcoolgroup.facebookhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* Called when the user clicks the login button */
+    public void loginRequest(View view) {
+        // Spawn activity to get login details
     }
 }
