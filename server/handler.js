b96a7f389f5c8ea6369489c37b376f6fd7c722fa
giuliojiang <giuliojiang@gmail.com>
2017-03-11 15:01:08 +0000

    done handlers to get list of movies

--- server/handler.js
+++ server/handler.js
@@ -12,6 +12,7 @@ var messages_authenticated = new Set([
     "my_movies"
 ]);
 
+// First order handlers
 // =============================================================================
 var handle = function(client, msgobj) {
     var type = msgobj["type"];
@@ -34,26 +35,62 @@ var handle_unauthenticated_msg = function(client, msgobj) {
     var type = msgobj["type"];
     
     if (type == "login") {
-        handle_login(client, msgobj, function(err, res) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            if (res) {
-                console.log(res);
-                return;
-            }
-        });
+        handle_login(client, msgobj, generic_handler_callback);
     }
 }
 
+
+
+var handle_authenticated_msg = function(client, msgobj) {
+    var type = msgobj["type"];
+    
+    var token = msgobj["token"];
+    if (!token || !utils_base.is_string(token)) {
+        console.log("no token field");
+        return;
+    }
+    
+    var username = session.validate_token(token);
+    if (!username) {
+        console.log("no valid token");
+        var resp = {
+            "type": "invalid_token"
+        };
+        socketio.send(client, JSON.stringify(resp));
+        return;
+    }
+    
+    if (type == "my_movies") {
+        handle_my_movies(username, client, msgobj, generic_handler_callback);
+    }
+}
+
+// Unauthenticated handlers
 // =============================================================================
 
 var handle_login = function(client, msgobj, callback) {
     console.log('handling login');
     // NOTE this login function doesn't authenticate, it always allows to login successfully
+    
     var username = msgobj["username"];
+    if (!username || !utils_base.is_string(username)) {
+        console.log("no username");
+        var resp = {
+            "type": "login_fail"
+        };
+        socketio.send(client, JSON.stringify(resp));
+        return;
+    }
+    
     var password = msgobj["password"];
+    if (!password || !utils_base.is_string(password)) {
+        console.log("no password");
+        var resp = {
+            "type": "login_fail"
+        };
+        socketio.send(client, JSON.stringify(resp));
+        return;
+    }
     
     var token = session.make_token(username);
     
@@ -64,6 +101,96 @@ var handle_login = function(client, msgobj, callback) {
     }
     socketio.send(client, JSON.stringify(resp));
 };
+
+// Authenticated handlers
+// =============================================================================
+
+var handle_my_movies = function(username, client, msgobj, callback) {
+    async.waterfall([
+    
+        // query database for movies
+        function(callback) {
+            db.user_movie.find({
+                "nickname": username
+            }, function(err, docs) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(null, docs);
+            });
+        },
+        
+        // application-level join for the titles
+        function(docs, callback) {
+            var movie_titles = [];
+            async.each(docs, function(doc, callback) {
+                var movieid = doc["movieid"];
+                db.movies.find({
+                    "_id": movieid
+                }, function(err, docs) {
+                    if (err) {
+                        callback(err);
+                        return;
+                    }
+                    if (docs.length == 1) {
+                        var doc = docs[0];
+                        var title = doc["title"];
+                        movie_titles.push(title);
+                        callback(null);
+                        return;
+                    } else {
+                        callback("expected only 1 result, instead " + docs.length);
+                        return;
+                    }
+                });
+            }, function(err) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(null, movie_titles);
+            });
+        },
+        
+        // send response
+        function(movie_titles, callback) {
+
+            var resp = {};
+            resp["type"] = "my_movies";
+            
+            var movies = [];
+            for (var i = 0; i < movie_titles.length; i++) {
+                var movieobj = {};
+                movieobj.title = movie_titles[i];
+                movies.push(movieobj);
+            }
+            resp["movies"] = movies;
+            
+            socketio.send(client, JSON.stringify(resp));
+        }
+    
+    
+    ], function(err, res) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+}
+
+// Helpers
+// =============================================================================
+var generic_handler_callback = function(err, res) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    if (res) {
+        console.log(res);
+        return;
+    }
+}
 
 module.exports = {
     "handle": handle
